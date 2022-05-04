d8494e51d3e05bb77b3e5262b03ef6df59050cac
Turkeyo <89434210+Turkeyo@users.noreply.github.com>
2022-05-04 11:06:56 +0800

    HW10

--- HW10/app/src/main/java/com/example/hw10/MainActivity.kt
+++ HW10/app/src/main/java/com/example/hw10/MainActivity.kt
@@ -11,11 +11,16 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //取得按鈕元素
         var btn = findViewById<Button>(R.id.btn)
 
+        //按鈕觸發事件
         btn.setOnClickListener{
+            //執行MyService.kt
             startService(Intent(this,MyService::class.java))
+            //顯示訊息
             Toast.makeText(this,"啟動Service",Toast.LENGTH_SHORT)
+            //關閉程式
             finish()
         }
     }
--- HW10/app/src/main/java/com/example/hw10/MyService.kt
+++ HW10/app/src/main/java/com/example/hw10/MyService.kt
@@ -11,11 +11,15 @@ class MyService : Service() {
         Thread(Runnable
         {
             try {
-                Thread.sleep(5000) //讓城市休息5秒
+                //讓程式休息5秒
+                Thread.sleep(5000)
+                //設定跳轉MainActivity2.kt的變數
                 val intent = Intent(this@MyService, MainActivity2::class.java)
+                //設定標籤
                 intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                //開始執行
                 this@MyService.startActivity(intent)
-            } catch (e: InterruptedException) {
+            } catch (e: InterruptedException) {  //抓取錯誤
                 e.printStackTrace()
             }
         }).start()
